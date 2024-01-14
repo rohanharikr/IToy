a74ecb085d4d05f5a254f0013bc00d1262bd211f
rohanharikr <rohanharikumar80@gmail.com>
2024-01-15 02:45:48 +0530

    self destruct

--- Assets/IToy/Editor/Inspector.cs
+++ Assets/IToy/Editor/Inspector.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿using System.IO;
+using UnityEditor;
 using UnityEngine;
 
 [CustomEditor(typeof(IToyControl))]
@@ -6,17 +7,15 @@ public class ControlInspector : Editor
 {
     public override void OnInspectorGUI()
     {
-        //base.OnInspectorGUI();
-
         IToyControl control = (IToyControl)target;
+
+        // Create a texture. Texture size does not matter, since
+        // LoadImage will replace with the size of the incoming image.
+        Texture2D imageOne = new Texture2D(2, 2);
 
         using (new EditorGUILayout.HorizontalScope())
         {
-            // Create a texture. Texture size does not matter, since
-            // LoadImage will replace with the size of the incoming image.
-            Texture2D imageOne = new Texture2D(2, 2);
             ImageConversion.LoadImage(imageOne, control.Original);
-
             GUILayout.Box(imageOne, GUILayout.Width(212), GUILayout.Height(212));
             Texture2D imageTwo = control.Current;
             GUILayout.Box(imageTwo, GUILayout.Width(212), GUILayout.Height(212));
@@ -28,6 +27,16 @@ public class ControlInspector : Editor
         EditorGUILayout.Slider("Saturation", 50f, 0f, 100f);
 
         GUILayout.Button("Reset");
-        GUILayout.Button("Self Destruct");
+        
+        if(GUILayout.Button("Self Destruct"))
+        {
+            string currentAssetPath = AssetDatabase.GetAssetPath(control.Current);
+            string currentAssetDirPath = Path.GetDirectoryName(currentAssetPath);
+            AssetDatabase.DeleteAsset(currentAssetPath);
+            File.WriteAllBytes(Path.Combine(currentAssetDirPath, "cat.png"), control.Original);
+            string controlPath = AssetDatabase.GetAssetPath(control);
+            AssetDatabase.DeleteAsset(controlPath);
+            AssetDatabase.Refresh();
+        };
     }
 }

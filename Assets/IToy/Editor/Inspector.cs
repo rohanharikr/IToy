5b138e811e65a017daaf4f76b8e97738791afcb4
rohanharikr <rohanharikumar80@gmail.com>
2024-01-17 17:27:20 +0530

    refactor to processor

--- Assets/IToy/Editor/Inspector.cs
+++ Assets/IToy/Editor/Inspector.cs
@@ -10,29 +10,15 @@ namespace IToy
     {
         bool _isAdvancedExpanded = false;
 
-        Toy toy;
-        Texture2D logo;
-        Texture2D original;
+        Toy _toy;
+        Texture2D _logo;
+        Texture2D _original;
 
-        Texture2D preview;
+        Processor _processor;
 
         SerializedProperty _removeBackground;
 
-        #region Materials
-        Material _backgroundMat;
-        Material _flipHorizontalMat;
-        Material _flipVerticalMat;
-        Material _cropMat;
-        Material _brightnessMat;
-        Material _contrastMat;
-        Material _hueMat;
-        Material _saturationMat;
-        #endregion
-
-        private void OnEnable()
-        {
-            Init();
-        }
+        private void OnEnable() => Init();
 
         public override void OnInspectorGUI()
         {
@@ -40,7 +26,7 @@ namespace IToy
 
             using (new EditorGUILayout.HorizontalScope())
             {
-                GUILayout.Label(logo, GUILayout.Width(100), GUILayout.Height(100));
+                GUILayout.Label(_logo, GUILayout.Width(100), GUILayout.Height(100));
                 using (new EditorGUILayout.VerticalScope(GUILayout.ExpandHeight(true)))
                 {
                     EditorGUILayout.Space(20);
@@ -62,12 +48,12 @@ namespace IToy
 
                 using (new EditorGUILayout.VerticalScope())
                 {
-                    GUILayout.Label(original, GUILayout.Width(previewSize), GUILayout.Height(previewSize));
+                    GUILayout.Label(_original, GUILayout.Width(previewSize), GUILayout.Height(previewSize));
                     EditorGUILayout.LabelField("Original / オリジナル");
                 }
                 using (new EditorGUILayout.VerticalScope())
                 {
-                    GUILayout.Label(preview, GUILayout.Width(previewSize), GUILayout.Height(previewSize));
+                    GUILayout.Label(_processor.texture, GUILayout.Width(previewSize), GUILayout.Height(previewSize));
 
                     GUIStyle labelStyle = new(EditorStyles.label);
                     labelStyle.normal.textColor = new Color(242 / 255f, 109 / 255f, 28 / 255f);
@@ -107,7 +93,7 @@ namespace IToy
                     EditorGUILayout.LabelField("Reset the image and remove the toy");
                     if (GUILayout.Button("Self-Destruct / 自己破壊", GUILayout.ExpandWidth(false)))
                     {
-                        SelfDestruct(toy);
+                        
                     };
                 }
             }
@@ -128,61 +114,37 @@ namespace IToy
 
         void Update()
         {
-            byte[] file = preview.EncodeToPNG();
-            File.WriteAllBytes("Assets/Resources/cat.png", file);
-            AssetDatabase.Refresh();
+            string currentFileGUID = serializedObject.FindProperty("Current").stringValue;
+            string filePath = AssetDatabase.GUIDToAssetPath(currentFileGUID);
+            _processor.WriteToDisk(filePath);
         }
 
         void Init()
         {
-            toy = (Toy)target;
-            logo = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/IToy/Media/logo.png");
+            _toy = (Toy)target;
+            _logo = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/IToy/Media/logo.png");
             _removeBackground = serializedObject.FindProperty("RemoveBackground");
-
-            #region Init shaders
-            Shader backgroundShader = AssetDatabase.LoadAssetAtPath<Shader>("Assets/IToy/Shaders/Background.shader");
-            Shader flipHorizontalShader = AssetDatabase.LoadAssetAtPath<Shader>("Assets/IToy/Shaders/Transform/FlipHorizontal.shader");
-            Shader flipVerticalShader = AssetDatabase.LoadAssetAtPath<Shader>("Assets/IToy/Shaders/Transform/FlipVertical.shader");
-            Shader cropShader = AssetDatabase.LoadAssetAtPath<Shader>("Assets/IToy/Shaders/Transform/Crop.shader");
-            Shader brightnessShader = AssetDatabase.LoadAssetAtPath<Shader>("Assets/IToy/Shaders/Correction/Brightness.shader");
-            Shader contrastShader = AssetDatabase.LoadAssetAtPath<Shader>("Assets/IToy/Shaders/Correction/Contrast.shader");
-            Shader hueShader = AssetDatabase.LoadAssetAtPath<Shader>("Assets/IToy/Shaders/Correction/Hue.shader");
-            Shader saturationShader = AssetDatabase.LoadAssetAtPath<Shader>("Assets/IToy/Shaders/Correction/Saturation.shader");
-            _backgroundMat = new Material(backgroundShader);
-            _flipHorizontalMat = new Material(flipHorizontalShader);
-            _flipVerticalMat = new Material(flipVerticalShader);
-            _cropMat = new Material(cropShader);
-            _brightnessMat = new Material(brightnessShader);
-            _contrastMat = new Material(contrastShader);
-            _hueMat = new Material(hueShader);
-            _saturationMat = new Material(saturationShader);
-            #endregion
+            _processor = new Processor();
 
             // Create a texture. Texture size does not matter, since
             // LoadImage will replace with the size of the incoming image.
-            original = new Texture2D(0, 0);
+            _original = new Texture2D(0, 0);
 
-            ImageConversion.LoadImage(original, toy.Original);
+            ImageConversion.LoadImage(_original, _toy.Original);
 
             DrawPreview();
         }
 
         void DrawPreview()
         {
-            preview = original;
+            _processor.texture = _original;
 
             RectInt cropValues = serializedObject.FindProperty("Transform").FindPropertyRelative("Crop").rectIntValue;
             if (cropValues.x != 0 || cropValues.y != 0 || cropValues.width != 0 || cropValues.height != 0)
-            {
-                _cropMat.SetInteger("_Top", cropValues.x);
-                _cropMat.SetInteger("_Right", cropValues.y);
-                _cropMat.SetInteger("_Bottom", cropValues.width);
-                _cropMat.SetInteger("_Left", cropValues.height);
-               preview = Utility.ApplyShader(preview, _cropMat);
-            }
+                _processor.Crop(cropValues);
 
             int removeBackground = serializedObject.FindProperty("RemoveBackground").enumValueIndex;
-            if (removeBackground != (int)RemoveBackgroundOpts.None)
+            if (_toy.RemoveBackground != (int)RemoveBackgroundOpts.None)
             {
                 Color colorToRemove;
                 if(removeBackground == (int)RemoveBackgroundOpts.White)
@@ -192,61 +154,30 @@ namespace IToy
                 else
                     colorToRemove = serializedObject.FindProperty("RemoveBackgroundColor").colorValue;
 
-                _backgroundMat.SetColor("_RemoveColor", colorToRemove);
-                preview = Utility.ApplyShader(preview, _backgroundMat);
+                _processor.RemoveBackground(colorToRemove);
             }
 
-            if (toy.Transform.FlipHorizontal)
-                preview = Utility.ApplyShader(preview, _flipHorizontalMat);
+            if (_toy.Transform.FlipHorizontal)
+                _processor.FlipHorizontal();
 
-            if (toy.Transform.FlipVertical)
-                preview = Utility.ApplyShader(preview, _flipVerticalMat);
+            if (_toy.Transform.FlipVertical)
+                _processor.FlipVertical();
 
             int brightnessLevel = serializedObject.FindProperty("Correction").FindPropertyRelative("Brightness").intValue;
             if(brightnessLevel != 0)
-            {
-                _brightnessMat.SetInteger("_Brightness", brightnessLevel);
-                preview = Utility.ApplyShader(preview, _brightnessMat);
-            }
+                _processor.Brightness(brightnessLevel);
 
             int contrastLevel = serializedObject.FindProperty("Correction").FindPropertyRelative("Contrast").intValue;
             if (contrastLevel != 0)
-            {
-                _contrastMat.SetInteger("_Contrast", contrastLevel);
-                preview = Utility.ApplyShader(preview, _contrastMat);
-            }
+                _processor.Brightness(contrastLevel);
 
             int hueLevel = serializedObject.FindProperty("Correction").FindPropertyRelative("Hue").intValue;
             if (hueLevel != 0)
-            {
-                _hueMat.SetInteger("_Hue", hueLevel);
-                preview = Utility.ApplyShader(preview, _hueMat);
-            }
+                _processor.Brightness(hueLevel);
 
             int saturationLevel = serializedObject.FindProperty("Correction").FindPropertyRelative("Saturation").intValue;
             if (saturationLevel != 0)
-            {
-                _saturationMat.SetInteger("_Saturation", saturationLevel);
-                preview = Utility.ApplyShader(preview, _saturationMat);
-            }
-
-            _backgroundMat.RevertAllPropertyOverrides();
-            _cropMat.RevertAllPropertyOverrides();
-            _brightnessMat.RevertAllPropertyOverrides();
-            _contrastMat.RevertAllPropertyOverrides();
-            _hueMat.RevertAllPropertyOverrides();
-            _saturationMat.RevertAllPropertyOverrides();
-        }
-
-        void SelfDestruct(Toy toy)
-        {
-            //string currentAssetPath = AssetDatabase.GetAssetPath(toy.Current);
-            //string currentAssetDirPath = Path.GetDirectoryName(currentAssetPath);
-            //AssetDatabase.DeleteAsset(currentAssetPath);
-            //File.WriteAllBytes(Path.Combine(currentAssetDirPath, "cat.png"), toy.Original);
-            //string toyPath = AssetDatabase.GetAssetPath(toy);
-            //AssetDatabase.DeleteAsset(toyPath);
-            //AssetDatabase.Refresh();
+                _processor.Brightness(saturationLevel);
         }
     }
 }
